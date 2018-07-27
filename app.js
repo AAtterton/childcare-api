af4f2ef31f6014dad257e0cde11b56dd296cc6de
Andy Atterton <andy_atterton@yahoo.com>
2018-07-27 19:29:55 +0100

    Added loging backend

--- app.js
+++ app.js
@@ -15,58 +15,80 @@ mongoose.connect('mongodb://localhost/childcare');
 var db = mongoose.connection;
 
 // **Get routes**
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('use api end point');
 });
 
 //  Kids
 //Get all kids
-app.get('/api/kids', function(req, res) {
-  Kids.getKids(function(err, kids) {
+app.get('/api/kids', function (req, res) {
+  Kids.getKids(function (err, kids) {
     if (err) {
       throw err;
     }
+
     res.json(kids);
   });
 });
 
 //Get all kids names
-app.get('/api/kidsnames', function(req, res) {
-  Kids.getKidsNames(function(err, kidsnames) {
+app.get('/api/kidsnames', function (req, res) {
+  Kids.getKidsNames(function (err, kidsnames) {
     if (err) {
       throw err;
     }
+
     res.json(kidsnames);
   });
 });
 
 // Get a single kid by ID
-app.get('/api/kids/:_id', function(req, res) {
-  Kids.getKidById(req.params._id, function(err, kid) {
+app.get('/api/kids/:_id', function (req, res) {
+  Kids.getKidById(req.params._id, function (err, kid) {
     if (err) {
       throw err;
     }
+
     res.json(kid);
   });
 });
 
 // Parents
 // Get all parents
-app.get('/api/parents', function(req, res) {
-  Parents.getParents(function(err, parents) {
+app.get('/api/parents', function (req, res) {
+  Parents.getParents(function (err, parents) {
     if (err) {
       throw err;
     }
+
     res.json(parents);
   });
 });
 
 // Get a single parents by ID
-app.get('/api/parents/:_id', function(req, res) {
-  Parents.getParentById(req.params._id, function(err, parent) {
+app.get('/api/parents/:_id', function (req, res) {
+  Parents.getParentById(req.params._id, function (err, parent) {
     if (err) {
       throw err;
     }
+
+    res.json(parent);
+  });
+});
+
+// Get a single parents by username
+app.get('/api/parents/user_name/:user_name/passcode/:passcode', function (req, res) {
+  Parents.getParentPassCodeByUserName(req.params, function (err, parent) {
+    if (err) {
+      throw err;
+    } else {
+      if (parent.passcode === req.params.passcode) {
+        parent.passcode = true;
+      } else {
+        parent.passcode = false;
+      }
+    }
+
     res.json(parent);
   });
 });
@@ -74,68 +96,86 @@ app.get('/api/parents/:_id', function(req, res) {
 // Reports
 // Get all reports
 
-app.get('/api/reports', function(req, res) {
-  Reports.getReports(function(err, reports) {
+app.get('/api/reports', function (req, res) {
+  Reports.getReports(function (err, reports) {
     if (err) {
       throw err;
     }
+
     res.json(reports);
   });
 });
 
 // Get a single reports by ID
-app.get('/api/reports/:_id', function(req, res) {
-  Reports.getReportById(req.params._id, function(err, report) {
+app.get('/api/reports/:_id', function (req, res) {
+  Reports.getReportById(req.params._id, function (err, report) {
     if (err) {
       throw err;
     }
+
     res.json(report);
   });
 });
 
 // **Post routes**
 // Add kid
-app.post('/api/kids', function(req, res) {
+app.post('/api/kids', function (req, res) {
   var kid = req.body;
-  Kids.addKid(kid, function(err, kid) {
+  Kids.addKid(kid, function (err, kid) {
     if (err) {
       throw err;
     }
+
     res.json(kid);
   });
 });
 
 // Add report
-app.post('/api/reports', function(req, res) {
+app.post('/api/reports', function (req, res) {
   var report = req.body;
-  Reports.addReport(report, function(err, report) {
+  Reports.addReport(report, function (err, report) {
     if (err) {
       throw err;
     }
+
     res.json(report);
+  });
+});
+
+// Add parent
+app.post('/api/parents', function (req, res) {
+  var parent = req.body;
+  Parents.addParent(parent, function (err, kid) {
+    if (err) {
+      throw err;
+    }
+
+    res.json(parent);
   });
 });
 
 // **Put routes**
 // Update kid
-app.put('/api/kids/:_id', function(req, res) {
+app.put('/api/kids/:_id', function (req, res) {
   var id = req.params._id;
   var kid = req.body;
-  Kids.updateKid(id, kid, {}, function(err, kid) {
+  Kids.updateKid(id, kid, {}, function (err, kid) {
     if (err) {
       throw err;
     }
+
     res.json(kid);
   });
 });
 
 // **Delete routes**
-app.delete('/api/kids/:_id', function(req, res) {
+app.delete('/api/kids/:_id', function (req, res) {
   var id = req.params._id;
-  Kids.deleteKid(id, function(err, kid) {
+  Kids.deleteKid(id, function (err, kid) {
     if (err) {
       throw err;
     }
+
     res.json(kid);
   });
 });
--- models/parents.js
+++ models/parents.js
@@ -2,28 +2,48 @@ var mongoose = require('mongoose');
 
 //Schema
 var parentSchema = mongoose.Schema({
-  first_name:{
+  first_name: {
     type: String,
-    required: true
+    required: true,
   },
-  last_name:{
+  last_name: {
     type: String,
-    required: true
+    required: true,
   },
-  dob:{
+  dob: {
     type: String,
-    required: true
+    required: true,
+  },
+  user_name: {
+    type: String,
+    required: true,
+  },
+  passcode: {
+    type: String,
+    required: true,
   },
 });
 
 var Parents = module.exports = mongoose.model('Parents', parentSchema);
 
 // get parents
-module.exports.getParents = function(callback, limit){
+module.exports.getParents = function (callback, limit) {
   Parents.find(callback).limit(limit);
 };
 
 // get parent by ID
-module.exports.getParentById = function(id, callback){
+module.exports.getParentById = function (id, callback) {
   Parents.findById(id, callback);
+};
+
+// get parent passcode by username
+module.exports.getParentPassCodeByUserName = function (username, callback) {
+  let fields = { passcode: 1 };
+  let query = { user_name: username.user_name };
+  Parents.findOne(query, fields, callback);
+};
+
+// add parent
+module.exports.addParent = function (parent, callback) {
+  Parents.create(parent, callback);
 };
